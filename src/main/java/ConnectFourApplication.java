51bd270085476ea6082f924357b482a4f7ee4692
rg <raska.gergely@gmail.com>
2021-05-06 08:35:59 +0200

    Add ConnectFourApplication class (extends javafx.Application)

---
+++ src/main/java/ConnectFourApplication.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
+
+@Slf4j
+public class ConnectFourApplication extends Application {
+
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        log.info("Running applicaton...");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-
+        Application.launch(ConnectFourApplication.class, args);
     }
 }
