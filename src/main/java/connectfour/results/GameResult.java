ee6211da96f2f18b5e1eb82b938ceb0b1f2c2155
rg <raska.gergely@gmail.com>
2021-05-12 17:15:54 +0200

    Add gameResultDao

---
+++ src/main/java/connectfour/results/GameResult.java
@@ -0,0 +1,4 @@
+package connectfour.results;
+
+public class GameResult {
+}
---
+++ src/main/java/connectfour/results/GameResultDao.java
@@ -0,0 +1,33 @@
+package connectfour.results;
+
+import connectfour.utils.jpa.GenericJpaDao;
+
+import javax.persistence.Persistence;
+
+public class GameResultDao extends GenericJpaDao<GameResult> {
+
+
+    private static GameResultDao instance;
+
+    /**
+     * Constructs a {@code GenericJpaDao} object.
+     */
+    private GameResultDao() {
+        super(GameResult.class);
+    }
+
+    public static GameResultDao getInstance() {
+        if (instance != null) {
+            return instance;
+        }
+        instance = new GameResultDao();
+        instance.setEntityManager(Persistence.createEntityManagerFactory("jpa-persistence-unit-1").createEntityManager();
+        return instance;
+    }
+
+/*    public List<GameResult> findBest(int n) {
+        return entityManager.createQuery("SELECT r FROM GameResult r WHERE r.solved = true ORDER BY r.duration ASC, r.created DESC", GameResult.class)
+                .setMaxResults(n)
+                .getResultList();
+    }*/
+}
